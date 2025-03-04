ce8d160e9352eb85c86d52f9a95051cccb14e1b3
salahuddinjony <salahuddin0758@gmail.com>
2025-03-04 15:55:53 +0600

    assets start

--- lib/app/view/screens/splash/splash_screen.dart
+++ lib/app/view/screens/splash/splash_screen.dart
@@ -5,6 +5,10 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return const Scaffold();
+  return const Scaffold(
+    body: Column(
+
+    ),
+  );
   }
 }

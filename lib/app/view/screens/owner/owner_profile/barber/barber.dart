8e8e2e0a208fb84cc556729d5aa9490ac6914e90
salahuddinjony <salahuddin0758@gmail.com>
2025-03-07 12:33:29 +0600

    owner nav icon collect

---
+++ lib/app/view/screens/owner/owner_profile/barber/barber.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Barber extends StatelessWidget {
+  const Barber({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/app/view/screens/owner/owner_profile/my_feed/my_feed.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyFeed extends StatelessWidget {
+  const MyFeed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

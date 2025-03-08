c64063e340915c5a1c0c3d51b3c6c2abb07f1cc6
salahuddinjony <salahuddin0758@gmail.com>
2025-03-08 11:41:37 +0600

    change password complete

--- lib/app/view/screens/owner/owner_profile/settings/change_password/change_password_screen.dart
+++ lib/app/view/screens/owner/owner_profile/settings/change_password/change_password_screen.dart
@@ -1,10 +1,74 @@
+import 'package:barber_time/app/core/bottom_navbar.dart';
+import 'package:barber_time/app/utils/app_colors.dart';
+import 'package:barber_time/app/utils/app_strings.dart';
+import 'package:barber_time/app/view/common_widgets/curved_Banner_clipper/curved_banner_clipper.dart';
+import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
+import 'package:barber_time/app/view/common_widgets/custom_button/custom_button.dart';
+import 'package:barber_time/app/view/common_widgets/custom_from_card/custom_from_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChangePasswordScreen extends StatelessWidget {
   const ChangePasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.white50,
+
+      ///============================ Header ===============================
+      appBar: const CustomAppBar(
+        appBarContent: AppStrings.changePassword,
+        iconData: Icons.arrow_back,
+      ),
+      body: ClipPath(
+        clipper: CurvedBannerClipper(),
+        child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height / 1.5,
+            color: AppColors.normalHover,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  //current password
+                  CustomFromCard(
+                      isBorderColor: true,
+                      isPassword: true,
+                      title: AppStrings.currentPassword,
+                      hinText: AppStrings.enterCurrentPassword,
+                      controller: TextEditingController(),
+                      validator: (v) {}),
+
+                  //New Password
+                  CustomFromCard(
+                      isBorderColor: true,
+                      isPassword: true,
+                      title: AppStrings.newPassword,
+                      hinText: AppStrings.enterNewPassword,
+                      controller: TextEditingController(),
+                      validator: (v) {}),
+
+                  //retype password
+                  CustomFromCard(
+                      isBorderColor: true,
+                      isPassword: true,
+                      title: AppStrings.retypePassword,
+                      hinText: AppStrings.retypeNewPassword,
+                      controller: TextEditingController(),
+                      validator: (v) {}),
+                  SizedBox(
+                    height: 50.h,
+                  ),
+                  CustomButton(
+                    onTap: () {},
+                    fillColor: AppColors.white50,
+                    title: AppStrings.changePassword,
+                  )
+                ],
+              ),
+            )),
+      ),
+    );
   }
 }
--- lib/app/view/screens/owner/owner_profile/settings/privacy_policy/privacy_policy_screen.dart
+++ lib/app/view/screens/owner/owner_profile/settings/privacy_policy/privacy_policy_screen.dart
@@ -1,3 +1,7 @@
+import 'package:barber_time/app/utils/app_colors.dart';
+import 'package:barber_time/app/utils/app_strings.dart';
+import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
+import 'package:barber_time/app/view/common_widgets/custom_text/custom_text.dart';
 import 'package:flutter/material.dart';
 
 class PrivacyPolicyScreen extends StatelessWidget {
@@ -5,6 +9,24 @@ class PrivacyPolicyScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: AppColors.normalHover,
+      ///============================ Header ===============================
+      appBar: const CustomAppBar(
+        appBarContent: AppStrings.privacyPolicy,
+        iconData: Icons.arrow_back,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+        child: CustomText(
+            textAlign: TextAlign.start,
+            maxLines: 100,
+            fontWeight: FontWeight.w400,
+            fontSize: 16,
+            color: AppColors.white50,
+            text:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."),
+      ),
+    );
   }
 }

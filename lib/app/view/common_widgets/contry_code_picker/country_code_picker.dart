896941a8fb9b0affb8d4a8bc0a8303bb0ff9ea6c
salahuddinjony <salahuddin0758@gmail.com>
2025-03-11 12:51:34 +0600

    owner personal info section complete

---
+++ lib/app/view/common_widgets/contry_code_picker/country_code_picker.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
+class CountryCodePickerField extends StatefulWidget {
+  final bool readOnly;
+  final Function onTap;
+
+  const CountryCodePickerField({
+    Key? key,
+    required this.readOnly,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  _CountryCodePickerFieldState createState() => _CountryCodePickerFieldState();
+}
+
+class _CountryCodePickerFieldState extends State<CountryCodePickerField> {
+  PhoneNumber number = PhoneNumber(isoCode: 'US'); // Default to 'US' (United States)
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        widget.onTap(); // Trigger the onTap function if needed
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Country Code Picker
+          InternationalPhoneNumberInput(
+            onInputChanged: (PhoneNumber number) {
+              setState(() {
+                this.number = number;
+              });
+              // Update the controller text with the selected country code and phone number
+              controller.text = "${number.dialCode} ${number.phoneNumber}";
+            },
+            onInputValidated: (bool value) {},
+            initialValue: number,
+            selectorConfig: const SelectorConfig(
+              selectorType: PhoneInputSelectorType.DIALOG,
+              setSelectorButtonAsPrefixIcon: true,
+            ),
+            textFieldController: controller,
+            inputDecoration: InputDecoration(
+              hintText: 'Enter phone number',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.black),
+              ),
+            ),
+            inputBorder: InputBorder.none,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/app/view/screens/owner/owner_profile/personal_info/edit_owner_profile.dart
+++ lib/app/view/screens/owner/owner_profile/personal_info/edit_owner_profile.dart
@@ -1,23 +1,23 @@
+import 'package:barber_time/app/core/bottom_navbar.dart';
 import 'package:barber_time/app/utils/app_colors.dart';
 import 'package:barber_time/app/utils/app_constants.dart';
 import 'package:barber_time/app/utils/app_strings.dart';
 import 'package:barber_time/app/view/common_widgets/curved_Banner_clipper/curved_banner_clipper.dart';
 import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
+import 'package:barber_time/app/view/common_widgets/custom_button/custom_button.dart';
 import 'package:barber_time/app/view/common_widgets/custom_from_card/custom_from_card.dart';
 import 'package:barber_time/app/view/common_widgets/custom_network_image/custom_network_image.dart';
 import 'package:barber_time/app/view/common_widgets/custom_radio_button/custom_radio_button.dart';
 import 'package:barber_time/app/view/common_widgets/custom_text/custom_text.dart';
-import 'package:barber_time/app/view/common_widgets/custom_text_field/custom_text_field.dart';
-import 'package:barber_time/app/view/screens/owner/owner_profile/personal_info/controller/owner_profile_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 class EditOwnerProfile extends StatelessWidget {
-  EditOwnerProfile({
+  const EditOwnerProfile({
     super.key,
   });
-
-  final OwnerProfileController controller = Get.find<OwnerProfileController>();
 
   @override
   Widget build(BuildContext context) {
@@ -30,85 +30,129 @@ class EditOwnerProfile extends StatelessWidget {
       ),
 
       ///============================ body ===============================
-      body: ClipPath(
-        clipper: CurvedBannerClipper(),
-        child: Container(
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xCCEDC4AC),
-                Color(0xFFE9874E),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+      body: SingleChildScrollView(
+        child: ClipPath(
+          clipper: CurvedBannerClipper(),
+          child: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xCCEDC4AC),
+                  Color(0xFFE9874E),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                    child: CustomNetworkImage(
-                        boxShape: BoxShape.circle,
-                        imageUrl: AppConstants.demoImage,
-                        height: 102,
-                        width: 102)),
-                //name
-                CustomFromCard(
-                    isBgColor: true,
-                    isBorderColor: true,
-                    title: AppStrings.name,
-                    controller: TextEditingController(text: " james"),
-                    validator: (f) {}),
-                //dateOfBirth
-                CustomFromCard(
-                    suffixIcon: const Icon(
-                      Icons.calendar_month,
-                      color: AppColors.white50,
-                    ),
-                    isRead: true,
-                    isBgColor: true,
-                    isBorderColor: true,
-                    title: AppStrings.dateOfBirth,
-                    controller: TextEditingController(text: '22/10/2024l'),
-                    validator: (f) {}),
-                const CustomText(
-                  color: AppColors.black,
-                  text: AppStrings.gender,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                  bottom: 8,
-                ),
-                //Gender
-                CustomRadioButtonRow(
-                  genderController: TextEditingController(),
-                ),
-                //======================Phone Number=================
-                const CustomText(
-                  color: AppColors.black,
-                  text: AppStrings.phoneNumber,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                  bottom: 8,
-                ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                      child: CustomNetworkImage(
+                          boxShape: BoxShape.circle,
+                          imageUrl: AppConstants.demoImage,
+                          height: 102,
+                          width: 102)),
+                  //name
+                  CustomFromCard(
+                      isBgColor: true,
+                      isBorderColor: true,
+                      title: AppStrings.name,
+                      controller: TextEditingController(text: " james"),
+                      validator: (f) {}),
+                  //dateOfBirth
+                  CustomFromCard(
+                      suffixIcon: const Icon(
+                        Icons.calendar_month,
+                        color: AppColors.white50,
+                      ),
+                      isRead: true,
+                      isBgColor: true,
+                      isBorderColor: true,
+                      title: AppStrings.dateOfBirth,
+                      controller: TextEditingController(text: '22/10/2024l'),
+                      validator: (f) {}),
+                  const CustomText(
+                    color: AppColors.black,
+                    text: AppStrings.gender,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    bottom: 8,
+                  ),
+                  //Gender
+                  CustomRadioButtonRow(
+                    genderController: TextEditingController(),
+                  ),
+                  //======================Phone Number=================
+                  const CustomText(
+                    color: AppColors.black,
+                    text: AppStrings.phoneNumber,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    bottom: 8,
+                  ),
 
-                const Row(
-                  children: [
-                    Expanded(flex: 3, child: CustomTextField()),
-                    SizedBox(width: 8,),
-                    Expanded(flex: 7, child: CustomTextField()),
-                  ],
-                ),
-                //location
-                CustomFromCard(
-                    isBgColor: true,
-                    isBorderColor: true,
-                    title: AppStrings.location,
-                    controller: TextEditingController(text: 'Abu Dhabi'),
-                    validator: (f) {}),
-              ],
+                  // CountryCodePickerField(
+                  //   readOnly: true, // Set to false if you want the user to be able to edit the phone number directly
+                  //   onTap: () {
+                  //     // Handle tap action (e.g., show dialog or perform other actions)
+                  //     print("Country code picker tapped!");
+                  //   },
+                  // ),
+                  InternationalPhoneNumberInput(
+                    onInputChanged: (PhoneNumber number) {
+                      debugPrint('Phone number: ${number.phoneNumber}');
+                    },
+                    onInputValidated: (bool value) {
+                      debugPrint('Is phone number valid: $value');
+                    },
+                    selectorConfig: const SelectorConfig(
+                      selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+                      useBottomSheetSafeArea: true,
+                    ),
+                    ignoreBlank: false,
+                    autoValidateMode: AutovalidateMode.disabled,
+                    selectorTextStyle: const TextStyle(color: Colors.black),
+                    // initialValue: ,
+                    textFieldController: TextEditingController(),
+                    formatInput: true,
+                    keyboardType: const TextInputType.numberWithOptions(
+                        signed: true, decimal: true),
+                    inputBorder: const OutlineInputBorder(),
+                    onSaved: (PhoneNumber number) {
+                      debugPrint('Saved phone number: ${number.phoneNumber}');
+                    },
+                  ),
+
+                  //location
+                  CustomFromCard(
+                      isBgColor: true,
+                      isBorderColor: true,
+                      title: AppStrings.location,
+                      controller: TextEditingController(text: 'Abu Dhabi'),
+                      validator: (f) {}),
+
+                  SizedBox(
+                    height: 20.h,
+                  ),
+
+                  //========================Save Button===============
+                  CustomButton(
+                    textColor: AppColors.white50,
+                    fillColor: AppColors.black,
+                    onTap: () {
+                      context.pop();
+                    },
+                    title: AppStrings.save,
+                  ),
+                  SizedBox(
+                    height: 100.h,
+                  ),
+                ],
+              ),
             ),
           ),
         ),

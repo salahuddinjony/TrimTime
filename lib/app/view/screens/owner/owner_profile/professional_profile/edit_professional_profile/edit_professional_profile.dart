6bb758e0a194cec822ae0659479ec5aca9990b75
salahuddinjony <salahuddin0758@gmail.com>
2025-03-17 12:23:25 +0600

    edit professional profile complete

--- lib/app/view/screens/owner/owner_profile/professional_profile/edit_professional_profile/edit_professional_profile.dart
+++ lib/app/view/screens/owner/owner_profile/professional_profile/edit_professional_profile/edit_professional_profile.dart
@@ -1,10 +1,179 @@
+import 'package:barber_time/app/utils/enums/user_role.dart';
+import 'package:barber_time/app/view/common_widgets/common_profile_total_card/common_profile_total_card.dart';
+import 'package:barber_time/app/view/common_widgets/custom_from_card/custom_from_card.dart';
+import 'package:barber_time/app/view/common_widgets/custom_text_field/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:barber_time/app/core/custom_assets/assets.gen.dart';
+import 'package:barber_time/app/utils/app_colors.dart';
+import 'package:barber_time/app/utils/app_constants.dart';
+import 'package:barber_time/app/utils/app_strings.dart';
+import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
+import 'package:barber_time/app/view/common_widgets/custom_network_image/custom_network_image.dart';
+import 'package:barber_time/app/view/common_widgets/custom_text/custom_text.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class EditProfessionalProfile extends StatelessWidget {
   const EditProfessionalProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final userRole = GoRouter.of(context).state.extra as UserRole?;
+
+    debugPrint("===================${userRole?.name}");
+    if (userRole == null) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+        body: const Center(child: Text('No user role received')),
+      );
+    }
+    return Scaffold(
+      backgroundColor: AppColors.linearFirst,
+      //==================✅✅Header✅✅===================
+      appBar: const CustomAppBar(
+        appBarBgColor: AppColors.linearFirst,
+        appBarContent: "Edit Barber Profile",
+        iconData: Icons.arrow_back,
+      ),
+
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 60),
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.1),
+                            blurRadius: 8,
+                            spreadRadius: 2,
+                            offset: const Offset(0, 8),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 50),
+                          CustomFromCard(
+                            title:AppStrings.name,
+                            controller: TextEditingController(),
+                            validator: (v) {},
+                          ),
+                          SizedBox(height: 20),
+                          CustomFromCard(
+                            title:"Bio",
+                            maxLine: 5,
+                            controller: TextEditingController(),
+                            validator: (v) {},
+                          ),
+                          SizedBox(height: 15),
+
+                          CustomFromCard(
+                            title:"Experience",
+                            controller: TextEditingController(),
+                            validator: (v) {},
+                          ),
+                          SizedBox(height: 15),
+
+                          CustomFromCard(
+                            title:"Current Work",
+                            controller: TextEditingController(),
+                            validator: (v) {},
+                          ),
+                          SizedBox(height: 15),
+
+                          CustomFromCard(
+                            title:"Add Skill",
+                            controller: TextEditingController(),
+                            validator: (v) {},
+                          ),
+                          SizedBox(height: 15),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          //==================✅✅Image✅✅===================
+                          CustomNetworkImage(
+                            imageUrl: AppConstants.demoImage,
+                            height: 100,
+                            width: 100,
+                            boxShape: BoxShape.circle,
+                          ),
+                          Positioned(
+                            right: 0,
+                            bottom: 0,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: const CircleAvatar(
+                                radius: 16,
+                                backgroundColor: AppColors.black,
+                                child: Icon(
+                                  Icons.camera_alt_outlined,
+                                  size: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //follow button and chart button
+  Widget _customButton(String text, IconData icon) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: BoxDecoration(
+        color: AppColors.black,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, size: 14, color: Colors.white),
+          const SizedBox(width: 6),
+          CustomText(
+            text: text,
+            fontSize: 12,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _iconButton(Widget icon) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: AppColors.black,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: icon,
+    );
   }
 }

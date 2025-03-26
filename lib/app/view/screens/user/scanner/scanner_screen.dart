31225bb6051e1e03c4d8cfb7fb147160bb962e69
salahuddinjony <salahuddin0758@gmail.com>
2025-03-26 14:53:21 +0600

    fix this

--- lib/app/view/screens/user/scanner/scanner_screen.dart
+++ lib/app/view/screens/user/scanner/scanner_screen.dart
@@ -1,13 +1,23 @@
 import 'package:barber_time/app/core/bottom_navbar.dart';
+import 'package:barber_time/app/core/custom_assets/assets.gen.dart';
 import 'package:barber_time/app/utils/app_colors.dart';
 import 'package:barber_time/app/utils/app_strings.dart';
 import 'package:barber_time/app/utils/enums/user_role.dart';
 import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
+import 'package:barber_time/app/view/common_widgets/custom_text/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScannerScreen extends StatelessWidget {
+class ScannerScreen extends StatefulWidget {
   const ScannerScreen({super.key});
+
+  @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  String scannedData = "No QR code scanned yet";
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +29,79 @@ class ScannerScreen extends StatelessWidget {
         body: const Center(child: Text('No user role received')),
       );
     }
+
     return Scaffold(
       bottomNavigationBar: BottomNavbar(currentIndex: 2, role: userRole),
-      backgroundColor: AppColors.white50,
+      backgroundColor: AppColors.linearFirst,
       appBar: const CustomAppBar(
         appBarContent: AppStrings.scanQrCode,
         appBarBgColor: AppColors.linearFirst,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // QR Scanner Box
+          Container(
+            height: 250,
+            width: 250,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 2),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: MobileScanner(
+              onDetect: (capture) {
+                final List<Barcode> barcodes = capture.barcodes;
+                for (final barcode in barcodes) {
+                  setState(() {
+                    scannedData = barcode.rawValue ?? "Invalid QR Code";
+                  });
+                }
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+
+          // Scanned Data Text
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: AppColors.normalHover,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: CustomText(
+              text: "Scanned Data: $scannedData",
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+              color: Colors.white,
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          // Scanner Icon with Label
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: AppColors.normalHover,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Assets.images.scanner.image(),
+                CustomText(
+                  left: 10,
+                  text: AppStrings.scanQrCode,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

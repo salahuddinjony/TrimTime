c82d7a27150a26a81a93644ddafbc807ad0f2875
salahuddinjony <salahuddin0758@gmail.com>
2025-04-26 17:13:00 +0600

    map screen complete

--- lib/app/view/screens/user/home/inner_screens/map_view_screen.dart
+++ lib/app/view/screens/user/home/inner_screens/map_view_screen.dart
@@ -2,8 +2,6 @@ import 'package:barber_time/app/utils/app_colors.dart';
 import 'package:barber_time/app/utils/app_strings.dart';
 import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 class MapViewScreen extends StatefulWidget {
@@ -14,49 +12,7 @@ class MapViewScreen extends StatefulWidget {
 }
 
 class _MapViewScreenState extends State<MapViewScreen> {
-  GoogleMapController? _mapController;
-  LatLng? _currentPosition;
 
-  @override
-  void initState() {
-    super.initState();
-    _determinePosition();
-  }
-
-  Future<void> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Check if location service is enabled
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    // Check for permission
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied.');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error('Location permissions are permanently denied.');
-    }
-
-    // Get the current location
-    final position = await Geolocator.getCurrentPosition();
-    setState(() {
-      _currentPosition = LatLng(position.latitude, position.longitude);
-    });
-
-    // Move camera to current position
-    _mapController?.animateCamera(
-      CameraUpdate.newLatLngZoom(_currentPosition!, 16),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,20 +23,7 @@ class _MapViewScreenState extends State<MapViewScreen> {
         appBarBgColor: AppColors.linearFirst,
         iconData: Icons.arrow_back,
       ),
-      body: _currentPosition == null
-          ? const Center(child: CircularProgressIndicator())
-          : GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: CameraPosition(
-          target: _currentPosition!,
-          zoom: 16,
-        ),
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        onMapCreated: (GoogleMapController controller) {
-          _mapController = controller;
-        },
-      ),
+
     );
   }
 }

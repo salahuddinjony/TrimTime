fdf1c50fccaf39411d926173bf389e0d82dd9bf5
salahuddinjony <salahuddin0758@gmail.com>
2025-05-10 16:04:32 +0600

    today working start

--- lib/app/view/screens/owner/owner_profile/personal_info/personal_info.dart
+++ lib/app/view/screens/owner/owner_profile/personal_info/personal_info.dart
@@ -5,11 +5,13 @@ import 'package:barber_time/app/utils/app_colors.dart';
 import 'package:barber_time/app/utils/app_constants.dart';
 import 'package:barber_time/app/utils/app_strings.dart';
 import 'package:barber_time/app/utils/enums/user_role.dart';
+import 'package:barber_time/app/view/common_widgets/common_home_app_bar/common_home_app_bar.dart';
 import 'package:barber_time/app/view/common_widgets/curved_Banner_clipper/curved_banner_clipper.dart';
 import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
 import 'package:barber_time/app/view/common_widgets/custom_network_image/custom_network_image.dart';
 import 'package:barber_time/app/view/common_widgets/custom_text/custom_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../common_widgets/custom_menu_card/custom_menu_card.dart';
@@ -32,14 +34,9 @@ class PersonalInfo extends StatelessWidget {
     }
     return Scaffold(
       ///============================ Header ===============================
-      appBar: CustomAppBar(
+      appBar: const CustomAppBar(
         appBarContent: AppStrings.profile,
         iconData: Icons.arrow_back,
-        isIcon: true,
-        onTap: () {
-          AppRouter.route
-              .pushNamed(RoutePath.editOwnerProfile, extra: userRole);
-        },
         appBarBgColor: AppColors.linearFirst,
       ),
 
@@ -84,37 +81,103 @@ class PersonalInfo extends StatelessWidget {
                       text: "Jane@example.com",
                       fontWeight: FontWeight.w400,
                       fontSize: 12,
+                      bottom: 10,
                       color: AppColors.black,
                     ),
                   ],
                 )),
+
+                Align(
+                  alignment: Alignment.center,
+                  child: GestureDetector(
+                    onTap: () {
+                      AppRouter.route.pushNamed(RoutePath.editOwnerProfile,
+                          extra: userRole);
+                    },
+                    child: Container(
+                      width: 130.w,
+                      padding: EdgeInsets.all(10.r),
+                      decoration: BoxDecoration(
+                          color: AppColors.white,
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Row(
+                        children: [
+                          CustomText(
+                            textAlign: TextAlign.center,
+                            text: AppStrings.editProfile,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14.sp,
+                            color: AppColors.secondary,
+                            left: 8,
+                            right: 8,
+                          ),
+                          Assets.icons.edit.svg()
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.h,),
+                Container(
+                  width: 100.w,
+                  padding: EdgeInsets.all(10.r),
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: [
+                      CustomText(
+                        text: 'Following',
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w300,
+                        color: AppColors.gray500,
+                      ),
+                      const Divider(),
+                      CustomText(
+                        text: '10k',
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w300,
+                        color: AppColors.gray500,
+                      ),
+                    ],
+                  ),
+                ),
                 //TOdo=====name====
                 CustomMenuCard(
                   onTap: () {},
                   isArrow: true,
                   text: "james",
-                  icon: Assets.icons.personalInfo.svg(  colorFilter: const ColorFilter.mode(
-                      AppColors.black, BlendMode.srcIn),),
+                  icon: Assets.icons.personalInfo.svg(
+                    colorFilter: const ColorFilter.mode(
+                        AppColors.black, BlendMode.srcIn),
+                  ),
                 ),
                 //=====date====
                 CustomMenuCard(
                   isArrow: true,
                   text: "22-03-1998",
-                  icon: Assets.icons.date.svg(  colorFilter: const ColorFilter.mode(
-                      AppColors.black, BlendMode.srcIn),),
+                  icon: Assets.icons.date.svg(
+                    colorFilter: const ColorFilter.mode(
+                        AppColors.black, BlendMode.srcIn),
+                  ),
                 ),
                 //=====gender====
                 CustomMenuCard(
                   isArrow: true,
                   text: "male",
-                  icon: Assets.icons.gender.svg(  colorFilter: const ColorFilter.mode(
-                      AppColors.black, BlendMode.srcIn),),
+                  icon: Assets.icons.gender.svg(
+                    colorFilter: const ColorFilter.mode(
+                        AppColors.black, BlendMode.srcIn),
+                  ),
                 ),
                 //=========phone===
                 CustomMenuCard(
                   text: '+4412451211',
-                  icon: Assets.icons.phone.svg(  colorFilter: const ColorFilter.mode(
-                      AppColors.black, BlendMode.srcIn),),
+                  icon: Assets.icons.phone.svg(
+                    colorFilter: const ColorFilter.mode(
+                        AppColors.black, BlendMode.srcIn),
+                  ),
                   isArrow: true,
                 ),
                 //=====location====

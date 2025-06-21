5a5667e17205ee8573266e0522cd5917a54a226b
salahuddinjony <salahuddin0758@gmail.com>
2025-06-21 20:39:19 +0600

    start invoice

--- lib/app/view/screens/owner/owner_profile/owner_payment/hiring_barber/hiring_barber.dart
+++ lib/app/view/screens/owner/owner_profile/owner_payment/hiring_barber/hiring_barber.dart
@@ -1,20 +1,38 @@
+import 'package:barber_time/app/utils/app_colors.dart';
 import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../../../core/custom_assets/assets.gen.dart';
+import '../../../../../../core/route_path.dart';
+import '../../../../../../core/routes.dart';
 import '../../../../../../utils/app_constants.dart';
+import '../../../../../../utils/enums/user_role.dart';
 import '../../../../../common_widgets/curved_Banner_clipper/curved_banner_clipper.dart';
 import '../../../../../common_widgets/custom_barber_card/custom_barber_card.dart';
-
+import '../../../../../common_widgets/custom_icon_button/custom_icon_button.dart';
 
 class HiringBarberPayment extends StatelessWidget {
   const HiringBarberPayment({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final userRole = GoRouter.of(context).state.extra as UserRole?;
+
+    debugPrint("===================${userRole?.name}");
+    if (userRole == null) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+        body: const Center(child: Text('No user role received')),
+      );
+    }
     return Scaffold(
-        appBar: const CustomAppBar(appBarContent: "Hiring Barber",iconData: Icons.arrow_back,),
-        body:
-        ClipPath(
+        appBar: const CustomAppBar(
+          appBarContent: "Hiring Barber",
+          iconData: Icons.arrow_back,
+        ),
+        body: ClipPath(
             clipper: CurvedBannerClipper(),
             child: Container(
               width: double.infinity,
@@ -27,20 +45,43 @@ class HiringBarberPayment extends StatelessWidget {
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                 ),
-              ),child:
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-              child: Column(
-                children: [
-                  CustomBarberCard(
-                    imageUrl: AppConstants.demoImage, // Barber's image URL
-                    name: 'Christian Ronaldo', // Barber's name
-                    role: 'Barber', // Barber's role
-                    contact: '+1 111 467 378 399', // Barber's contact number
-                  ),
-                ],
               ),
-            ),))
-    );
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
+                  children: [
+                    CustomBarberCard(
+                      imageUrl: AppConstants.demoImage, // Barber's image URL
+                      name: 'Christian Ronaldo', // Barber's name
+                      role: 'Barber', // Barber's role
+                      contact: '+1 111 467 378 399', // Barber's contact number
+                    ),
+                    SizedBox(height: 20.h,),
+                    CustomIconButton(
+                      iconColor: Colors.white,
+                      textColor: Colors.white,
+                      isBgColor: Colors.black,
+                      text: 'Cash Payment',
+                      iconPath: Assets.images.hugeiconsPayment02.path,
+                      onTap: () {},
+                    ),
+
+                    SizedBox(height: 20.h,),
+                    CustomIconButton(
+                      iconColor: Colors.white,
+                      textColor: Colors.white,
+                      isBgColor: AppColors.secondary,
+                      text: 'Online Payment',
+                      iconPath: Assets.images.hugeiconsPayment02.path,
+                      onTap: () {
+                        AppRouter.route.pushNamed(RoutePath.paymentOption,
+                            extra: userRole);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            )));
   }
 }
--- lib/app/view/screens/owner/owner_profile/owner_payment/owner_payment.dart
+++ lib/app/view/screens/owner/owner_profile/owner_payment/owner_payment.dart
@@ -1,9 +1,11 @@
 import 'package:barber_time/app/view/common_widgets/custom_appbar/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/route_path.dart';
 import '../../../../../core/routes.dart';
 import '../../../../../utils/app_constants.dart';
+import '../../../../../utils/enums/user_role.dart';
 import '../../../../common_widgets/curved_Banner_clipper/curved_banner_clipper.dart';
 import '../../../../common_widgets/custom_barber_card/custom_barber_card.dart';
 
@@ -12,42 +14,55 @@ class OwnerPayment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userRole = GoRouter.of(context).state.extra as UserRole?;
+
+    debugPrint("===================${userRole?.name}");
+    if (userRole == null) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+        body: const Center(child: Text('No user role received')),
+      );
+    }
     return Scaffold(
-      appBar: const CustomAppBar(appBarContent: "Barber",iconData: Icons.arrow_back,),
-      body:
-      ClipPath(
-        clipper: CurvedBannerClipper(),
-    child: Container(
-    width: double.infinity,
-    decoration: const BoxDecoration(
-    gradient: LinearGradient(
-    colors: [
-    Color(0xCCEDC4AC), // First color (with opacity)
-    Color(0xFFE9874E),
-    ],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-    ),
-    ),child:
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: (){
-                AppRouter.route.pushNamed(RoutePath.hiringBarberPayment,
-                    );
-              },
-              child: CustomBarberCard(
-                imageUrl: AppConstants.demoImage, // Barber's image URL
-                name: 'Christian Ronaldo', // Barber's name
-                role: 'Barber', // Barber's role
-                contact: '+1 111 467 378 399', // Barber's contact number
-              ),
-            ),
-          ],
+        appBar: const CustomAppBar(
+          appBarContent: "Barber",
+          iconData: Icons.arrow_back,
         ),
-      ),))
-    );
+        body: ClipPath(
+            clipper: CurvedBannerClipper(),
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xCCEDC4AC), // First color (with opacity)
+                    Color(0xFFE9874E),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        AppRouter.route.pushNamed(RoutePath.hiringBarberPayment,
+                            extra: userRole);
+                      },
+                      child: CustomBarberCard(
+                        imageUrl: AppConstants.demoImage, // Barber's image URL
+                        name: 'Christian Ronaldo', // Barber's name
+                        role: 'Barber', // Barber's role
+                        contact:
+                            '+1 111 467 378 399', // Barber's contact number
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )));
   }
 }

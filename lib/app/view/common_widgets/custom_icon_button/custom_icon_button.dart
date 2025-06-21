e894f55c9a42b8a480d80a0b6dbb787b9fb0ed75
salahuddinjony <salahuddin0758@gmail.com>
2025-06-21 16:01:56 +0600

    download button added

--- lib/app/view/common_widgets/custom_icon_button/custom_icon_button.dart
+++ lib/app/view/common_widgets/custom_icon_button/custom_icon_button.dart
@@ -8,33 +8,37 @@ class CustomIconButton extends StatelessWidget {
   final String text;
   final String iconPath;
   final double iconLeftPadding;
+  final VoidCallback onTap;
 
   const CustomIconButton({
     super.key,
     required this.text,
     required this.iconPath,
-    this.iconLeftPadding = 10,
+    this.iconLeftPadding = 10, required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15.r),
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          Image.asset(iconPath),
-          SizedBox(width: iconLeftPadding),
-          CustomText(
-            text: text,
-            fontWeight: FontWeight.w500,
-            fontSize: 16.sp,
-            color: AppColors.black,
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(15.r),
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Image.asset(iconPath),
+            SizedBox(width: iconLeftPadding),
+            CustomText(
+              text: text,
+              fontWeight: FontWeight.w500,
+              fontSize: 16.sp,
+              color: AppColors.black,
+            ),
+          ],
+        ),
       ),
     );
   }

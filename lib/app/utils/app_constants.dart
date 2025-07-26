cf07c4eda24458b6419019ce053bc2bc435891b0
masum <promotionbd321@gmail.com>
2025-07-26 16:32:22 +0600

    flutter build apk

--- lib/app/utils/app_constants.dart
+++ lib/app/utils/app_constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
 
   static String bearerToken = "BearerToken";
   static String dartAppbarImage = "https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/teaser_image/blog_entry/2023-07/Planet-Volumes-Unsplash.jpg?itok=2eUese1d";
-  static String demoImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgwVA_PGXVY8bwscWq0OhGBLgK9ahPe6sTzQ&s";
+  static String demoImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtS9E6lJh3EU8xu5l9rlgrAnPS9OsMqaY01YdXnHep5oOG_TSF55U5wyWU1eDoAWTrJMk&usqp=CAU";
   static String shop = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKSPQoSyVr6gEwSbCG9b4a-3nME0YXht3kAg&s";
   static String style1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5JgL9cfWjJ9vv2L9ZH9S8uV8AmqIRaW9CRw&s";
 

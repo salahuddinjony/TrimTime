6297a2918b89f2d7e88b765da12c1ad591949ef1
salahuddinjony <salahuddin0758@gmail.com>
2025-11-15 11:56:25 +0600

    added following and followers in realtime

--- lib/app/view/screens/owner/owner_profile/flowers/mixin_followers_following/mixin_followers_following.dart
+++ lib/app/view/screens/owner/owner_profile/flowers/mixin_followers_following/mixin_followers_following.dart
@@ -1,21 +1,24 @@
 import 'package:barber_time/app/services/api_client.dart';
 import 'package:barber_time/app/services/api_url.dart';
+import 'package:barber_time/app/view/screens/barber/barber_home/controller/mixin/mixin_selon_management.dart';
 import 'package:barber_time/app/view/screens/owner/owner_profile/flowers/model/followers_model.dart';
 import 'package:barber_time/app/view/screens/owner/owner_profile/following/model/following_model.dart';
 import 'package:get/get.dart';
 
-mixin MixinFollowersFollowing {
+mixin MixinFollowersFollowing on MixinSelonManagement{
   RxList<Follower> followersList = RxList<Follower>([]);
   RxList<Following> followingList = RxList<Following>([]);
-  Rx<RxStatus> followersStatus = Rx<RxStatus>(RxStatus.loading());
-  Rx<RxStatus> followingStatus = Rx<RxStatus>(RxStatus.loading());
+  Rx<RxStatus> followersStatus = Rx<RxStatus>(RxStatus.empty());
+  Rx<RxStatus> followingStatus = Rx<RxStatus>(RxStatus.empty());
 
-  Future<void> fetchFollowerOrFollowingData({required bool isFollowers}) async {
+  Future<void> fetchFollowerOrFollowingData({required bool isFollowers, bool needLoader=true}) async {
     try {
-      if (isFollowers) {
-        followersStatus.value = RxStatus.loading();
-      } else {
-        followingStatus.value = RxStatus.loading();
+      if(needLoader){
+        if (isFollowers) {
+          followersStatus.value = RxStatus.loading();
+        } else {
+          followingStatus.value = RxStatus.loading();
+        }
       }
       final url = isFollowers
           ? ApiUrl.fetchUserData(filter: 'followers')
@@ -44,4 +47,6 @@ mixin MixinFollowersFollowing {
       followingStatus.value = RxStatus.error(e.toString());
     } finally {}
   }
+
+  
 }

74d3b85da3ebcee171ce091f8e59dccdbc4eb321
salahuddinjony <salahuddin0758@gmail.com>
2025-11-16 18:19:57 +0600

    separte by widgets of the que management

--- lib/app/view/screens/owner/owner_que/controller/que_controller.dart
+++ lib/app/view/screens/owner/owner_que/controller/que_controller.dart
@@ -1,6 +1,6 @@
 import 'package:barber_time/app/view/screens/owner/owner_que/controller/mixin/mixin_get_barber_with_date_time/mixin_get_barber_with_date_time.dart';
 import 'package:barber_time/app/view/screens/owner/owner_que/controller/mixin/mixin_non_registered_bookings/mixin_non_registered_bookings.dart';
-import 'package:barber_time/app/view/screens/owner/owner_que/controller/mixin/mixin_que_management.dart';
+import 'package:barber_time/app/view/screens/owner/owner_que/controller/mixin/queue_management/mixin_que_management.dart';
 import 'package:barber_time/app/view/screens/owner/owner_que/controller/mixin/mixin_services/mixin/mixin_get_services.dart';
 import 'package:get/get.dart';
 

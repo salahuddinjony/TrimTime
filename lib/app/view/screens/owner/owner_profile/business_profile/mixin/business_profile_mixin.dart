eafb2f668afa962cc80a63f49c07ce93b92ad9d1
salahuddinjony <salahuddin0758@gmail.com>
2025-11-19 19:30:55 +0600

    update selon owner business profile

---
+++ lib/app/view/screens/owner/owner_profile/business_profile/mixin/business_profile_mixin.dart
@@ -0,0 +1,29 @@
+import 'package:barber_time/app/services/api_client.dart';
+import 'package:barber_time/app/services/api_url.dart';
+import 'package:barber_time/app/view/screens/owner/owner_profile/business_profile/model/business_profile_data.dart';
+import 'package:get/get.dart';
+
+mixin BusinessProfileMixin {
+  Rx<BusinessProfileData?> businessProfileData = Rx<BusinessProfileData?>(null);
+  Rx<RxStatus> businessProfileStatus = Rx<RxStatus>(RxStatus.empty());
+
+  Future<void> fetchBusinessProfiles() async {
+    try {
+      businessProfileStatus.value = RxStatus.loading();
+      final response = await ApiClient.getData(ApiUrl.businessProfile);
+
+      if (response.statusCode == 200) {
+        final data = response.body;
+        final responseObj = BusinessProfileResponse.fromJson(data);
+        businessProfileData.value = responseObj.data; 
+        businessProfileStatus.value = RxStatus.success();
+      } else {
+        businessProfileStatus.value =
+            RxStatus.error('Failed to fetch business profiles');
+      }
+    } catch (e) {
+      businessProfileStatus.value =
+          RxStatus.error('Failed to fetch business profiles');
+    }
+  }
+}
